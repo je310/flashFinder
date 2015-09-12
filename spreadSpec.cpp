966554cfcaa2ff1b3f6b6746498c65f6a15dcb00
twighk <twighk@outlook.com>
2015-09-12 15:34:16 +0100

    Output for spreadspec

--- spreadSpec.cpp
+++ spreadSpec.cpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 #include <cv.h>
+#include <opencv2/contrib/contrib.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <highgui.h>
 #include <unistd.h>
 
@@ -13,16 +16,6 @@
 //namespaces
 using namespace std;
 using namespace cv;
-
-vector<Mat> autoCorrelate (vector<Mat> input, size_t firstframe);
-void haveALook(int lengthOfBuffers, vector<Mat> corrBuffer, vector<Mat> imageBuffer,Mat av, const int derating);
-Mat findAvOfVid(string fileName,double decimation,int periodsToAverage,int lengthOfBuffers);
-vector<Point> findCodedness(vector<Mat> corrBuffer,string winName, double threshold, vector<double> codeSeries, bool veryCorr,double factorHigh, double factorLow, vector<Mat> &maxedCorr, Point clickedLocationOld);
-
-void CallBackFunc(int event, int x, int y, int flags, void* userdata);
-void drawGraph(vector<double> corrSeries, string WinName);
-
-template<unsigned int N> vector<double> corrCode(string bitstring, int derating);
 
 struct getFrameFunctor{
     double _decimation;
@@ -37,38 +30,21 @@ struct getFrameFunctor{
 
         cap >> frameIn;
         resize(frameIn,frameSmall,Size(0,0),_decimation,_decimation);
-        cv::cvtColor(frameSmall, frameGrey, CV_BGR2GRAY);
+        cvtColor(frameSmall, frameGrey, CV_BGR2GRAY);
         frameGrey.convertTo(framedouble,CV_64FC1);
 
         return framedouble;
     }
 };
 
-template <unsigned int N>
-vector<double> spreadGenerator(vector <size_t> XorFrom, int derating) {
-     vector<double> out;
-     bitset<N> initialState = 1;
-     bitset<N> lsfr = initialState;
 
-     for(size_t x = 0; x!= XorFrom.size(); x++)
-         XorFrom[x] -= 1;
+struct crosscorrstruct{
+	Mat Amplitude;
+	Mat Phase;
+};
 
-     do {
-         bool bit = lsfr[XorFrom[0]];
-         for ( size_t x = 1
-             ; x!= XorFrom.size()
-             ; x++
-             )
-            bit ^= lsfr[XorFrom[x]];
-         lsfr <<= 1;
-         lsfr[0] = bit;
-
-         for (int d = 0; d != derating; d++)
-			 out.push_back(bit);
-     } while(initialState != lsfr);
-
-     return out;
-}
+template <unsigned int N> vector<double> spreadGenerator(vector <size_t> XorFrom, int derating);
+crosscorrstruct crossCorr(vector <Mat> imageBuffer, vector<double> spreadcode);
 
 
 int main(){
@@ -107,9 +83,53 @@ int main(){
     }
     cout <<"set up video stream"<<endl;
     //declare variables
-    //namedWindow("looking",WINDOW_NORMAL );
 
     cv::vector <double> spreadcode = spreadGenerator<bits>(XorFrom, derating);
+
+    getFrameFunctor getFrame(decimation);
+    vector<Mat> imageBuffer(lengthOfBuffers);    //buffer with grey images
+    for(int i = 0; i < lengthOfBuffers; i++){
+        imageBuffer.at(i) = getFrame(cap);
+    }
+
+	crosscorrstruct results = crossCorr(imageBuffer, spreadcode);
+	
+	double Amin,Amax;
+	int x,y;
+
+	minMaxIdx(results.Amplitude, &Amin, &Amax, &x, &y); 
+
+	cout << " max: " << Amax << " maxframe: " << results.Phase.at<double>(y,x) << endl;
+
+	
+	namedWindow("Map", WINDOW_NORMAL);	
+	Mat phase;
+	results.Phase.convertTo(phase, CV_8UC1);
+	applyColorMap(phase/lengthOfBuffers*255.0, phase, COLORMAP_JET);
+	phase.convertTo(phase,CV_64FC3);
+	phase /= 255;
+
+	Mat amplitude = (results.Amplitude-Amin)/(Amax-Amin);
+	amplitude.convertTo(amplitude, CV_32FC1);
+	cvtColor(amplitude, amplitude, CV_GRAY2BGR);
+	amplitude.convertTo(amplitude, CV_64FC3);
+
+	cout << phase.type() << " " << amplitude.type() << endl;
+	cout << phase.channels() << " " << amplitude.channels() << endl;
+	cout << phase.cols << " " << amplitude.cols << endl;
+	cout << phase.rows << " " << amplitude.rows << endl;
+
+	imshow("Map", phase.mul(amplitude));//(results.Amplitude-Amin)/(Amax-Amin));
+
+	for (char k = '\0'; k != 'q'; k = waitKey(0));
+
+	return 0;
+}
+
+//function implementations
+
+crosscorrstruct crossCorr(vector <Mat> imageBuffer, vector<double> spreadcode){
+	// Fourier Transform Spreadcode
 	fftw_complex *spreadcodeftin, *spreadcodeftout;
 	spreadcodeftin  = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * spreadcode.size());
 	spreadcodeftout = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * spreadcode.size());
@@ -127,38 +147,36 @@ int main(){
 	spreadcodeftout[0][1] = 0;
 	fftw_destroy_plan(p);
 	fftw_free(spreadcodeftin); 
+	
+	crosscorrstruct out;
+	out.Amplitude = Mat::zeros(imageBuffer.at(0).size(), imageBuffer.at(0).type());
+	out.Phase     = Mat::zeros(imageBuffer.at(0).size(), imageBuffer.at(0).type());
 
-
-    getFrameFunctor getFrame(decimation);
-    vector<Mat> imageBuffer(lengthOfBuffers);    //buffer with grey images
-    for(int i = 0; i < lengthOfBuffers; i++){
-        imageBuffer.at(i) = getFrame(cap);
-    }
-
+	// Cross correlate
 	fftw_complex *pixelsftin, *pixelsftout;
-	pixelsftin  = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * lengthOfBuffers);
-	pixelsftout = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * lengthOfBuffers);
+	pixelsftin  = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * imageBuffer.size());
+	pixelsftout = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * imageBuffer.size());
 	fftw_plan(forward);
-	forward = fftw_plan_dft_1d( lengthOfBuffers
+	forward = fftw_plan_dft_1d( imageBuffer.size()
 						      , pixelsftin, pixelsftout
 						      , FFTW_FORWARD, FFTW_ESTIMATE
 						      );
 	fftw_plan(backward);
-	backward= fftw_plan_dft_1d( lengthOfBuffers
+	backward= fftw_plan_dft_1d( imageBuffer.size()
 						      , pixelsftin, pixelsftout
 						      , FFTW_BACKWARD, FFTW_ESTIMATE
 						      );
 
     for(int x = 0; x != imageBuffer.at(0).rows; x++){
         for(int y = 0; y != imageBuffer.at(0).cols; y++){
-            for(int i = 0; i < lengthOfBuffers; i++){
+            for(int i = 0; i < imageBuffer.size(); ++i){
                 pixelsftin[i][0] = imageBuffer.at(i).at<double>(x,y)/255.0;
                 pixelsftin[i][1] = 0;
             }
 
 			fftw_execute(forward);
 
-            for (int j = 0; j < lengthOfBuffers; j++){
+            for (int j = 0; j < imageBuffer.size(); ++j ){
                 complex<double> p (    pixelsftout[j][0], -pixelsftout    [j][1]);
                 complex<double> c (spreadcodeftout[j][0],  spreadcodeftout[j][1]);
                 complex<double> o = p*c;
@@ -168,8 +186,12 @@ int main(){
 
 			fftw_execute(backward);
 
-            for(int i = 0; i < lengthOfBuffers; i++){
+            for(int i = 0; i < imageBuffer.size(); ++i){
                 imageBuffer.at(i).at<double>(x,y) = pixelsftout[i][0];
+				if (pixelsftout[i][0] > out.Amplitude.at<double>(x,y)){
+					out.Amplitude.at<double>(x,y) = pixelsftout[i][0];
+					out.Phase.at<double>(x,y) = i;
+				}
             }
         }
     }
@@ -178,314 +200,34 @@ int main(){
 	fftw_destroy_plan(backward);
 	fftw_free(pixelsftin);
 	fftw_free(pixelsftout);
-
-	double gmax = 1e-8, gmin = 1e8;
-	int maxframe = 0;
-	for(int i = 0; i != lengthOfBuffers; i++){
-		double min, max;
-		minMaxLoc(imageBuffer.at(i), &min, &max);
-		if (min < gmin) gmin = min;
-		if (max > gmax) {
-			gmax = max;
-			maxframe = i;
-		}
-	}
-
-	cout << "min: " << gmin << " max: " << gmax << " maxframe: " << maxframe << endl;
-
 	fftw_free(spreadcodeftout);
-
-    haveALook(lengthOfBuffers,imageBuffer, imageBuffer,imageBuffer.at(0),derating);
-
-	return 0;
+	
+	return out;
 }
 
-//function implementations
+template <unsigned int N>
+vector<double> spreadGenerator(vector <size_t> XorFrom, int derating) {
+     vector<double> out;
+     bitset<N> initialState = 1;
+     bitset<N> lsfr = initialState;
 
-vector<Mat> autoCorrelate (vector<Mat> input, size_t firstframe){      //the ofset aims to allow a circular buffer use of the vector.
-    vector<Mat> corrResult(input.size());
-    int sz = input.size();
+     for(size_t x = 0; x!= XorFrom.size(); x++)
+         XorFrom[x] -= 1;
 
-#pragma omp parallel for
-    for(int j = 0; j < input.size(); j++){                                  //for each pair of images that align this time
-        Mat a = input.at((firstframe  )%sz);
-        Mat b = input.at((firstframe+j)%sz);
-        Mat c = a.mul(b);
-        corrResult.at(j)=c;
-    }
+     do {
+         bool bit = lsfr[XorFrom[0]];
+         for ( size_t x = 1
+             ; x!= XorFrom.size()
+             ; x++
+             )
+            bit ^= lsfr[XorFrom[x]];
+         lsfr <<= 1;
+         lsfr[0] = bit;
 
-    return corrResult;
-}
-void haveALook(int lengthOfBuffers, vector<Mat> corrBuffer, vector<Mat> imageBuffer,Mat av, const int derating){
-    namedWindow("corrBuffer",WINDOW_NORMAL );
-    namedWindow("averageImage",WINDOW_NORMAL );
-    namedWindow("heatMap",WINDOW_NORMAL );
-    namedWindow("heatMap2",WINDOW_NORMAL );
-    static bool veryCor = 0;
-    static double factorHigh = 0.05;
-    static double factorLow = 0.025;
-    Point clickLocation;
-    Point clickLocationOld;
-    clickLocationOld.x = 0;
-    clickLocationOld.y = 0;
-    vector<double> corrSeries;
-    vector<double> corrSeries2;
-    vector<Mat> maxedCorr;
-    for(int i =0; i < corrBuffer.size(); i++){
-            maxedCorr.push_back(Mat(corrBuffer.at(0).size(),CV_64FC1,0.0));
-    }
-    for(int i =0; i < corrBuffer.size(); i++){
-        corrSeries.push_back(0.0);
-        corrSeries2.push_back(0.0);
-    }
-    setMouseCallback("corrBuffer", CallBackFunc, &clickLocation);
-    setMouseCallback("imageBuffer", CallBackFunc, &clickLocation);
-    setMouseCallback("heatMap", CallBackFunc, &clickLocation);
-    setMouseCallback("heatMap2", CallBackFunc, &clickLocation);
-    setMouseCallback("averageImage", CallBackFunc, &clickLocation);
-    while(1){
-        char k =waitKey(1);
-        usleep(100000);
-        static int myWin =0;
-        if(k == 'j'){
-            myWin++;
-            if( myWin == lengthOfBuffers) myWin = lengthOfBuffers-1;
-            cout<<myWin<<endl;
-        }
-        if(k == 'k'){
-            myWin--;
-            if( myWin == -1) myWin = 0;
-            cout<<myWin<<endl;
-        }
-        if(k == 'h'){
-            vector<double> code = corrCode<8>("11110000", derating);
-            drawGraph(code, "the code");
-            findCodedness(corrBuffer, "heatMap", 0.1,code ,veryCor,factorHigh,factorLow,maxedCorr,clickLocationOld);
-        }
-        if(k == '='){
-            factorHigh +=0.005;
-            cout << "displaying only the best:"<< factorHigh<<endl;
-        }
-        if(k == '-'){
-            factorHigh -=0.005;
-            cout << "displaying only the best:"<< factorHigh<<endl;
-        }
-        if(k == '0'){
-            factorLow +=0.005;
-            cout << "displaying only the worst:"<< factorLow<<endl;
-        }
-        if(k == '9'){
-            factorLow -=0.005;
-            cout << "displaying only the worst:"<< factorLow<<endl;
-        }
-        if(clickLocation.x != clickLocationOld.x || clickLocation.y != clickLocationOld.y){
-            for(int i =0; i < corrBuffer.size(); i++){
-                corrSeries.at(i) = corrBuffer.at(i).at<double>(clickLocation.y,clickLocation.x);
-            }
-            for(int i =0; i < corrBuffer.size(); i++){
-                corrSeries2.at(i) = maxedCorr.at(i).at<double>(clickLocation.y,clickLocation.x);
-            }
-            drawGraph(corrSeries, "inspectSeries");
-            drawGraph(corrSeries2, "maxed");
-            clickLocationOld = clickLocation;
-        }
-        if(k == 'q') break;
-        if(k == 'd'){
-            veryCor = ! veryCor;
-        }
-        imshow("corrBuffer",corrBuffer.at(myWin)/(255));
-        imshow("averageImage",av/255);
-    }
-    return;
+         for (int d = 0; d != derating; d++)
+			 out.push_back(bit);
+     } while(initialState != lsfr);
+
+     return out;
 }
 
-Mat findAvOfVid(string fileName,double decimation,int periodsToAverage,int lengthOfBuffers){
-    VideoCapture cap(fileName.c_str());
-    getFrameFunctor getFrame(decimation);
-
-    int NoOfFrames = periodsToAverage*lengthOfBuffers;
-    if(periodsToAverage == 0){
-        NoOfFrames = cap.get(CV_CAP_PROP_FRAME_COUNT);
-    }
-
-    Mat frame = getFrame(cap);
-    Mat averagedoubleIn(frame.size(),CV_64FC1,0.0);
-    frame.copyTo(averagedoubleIn);
-    for(int i = 1; i < NoOfFrames; i++){
-        frame = getFrame(cap);
-        averagedoubleIn += frame;
-    }
-    averagedoubleIn = averagedoubleIn/NoOfFrames;
-    return averagedoubleIn;
-}
-
-void CallBackFunc(int event, int x, int y, int flags, void* clickLocation){
-    Point* thisLocation = (Point*) clickLocation;
-    if  ( event == EVENT_LBUTTONDOWN ){
-        cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-        thisLocation->x = x;
-        thisLocation->y = y;
-    }
-    else if  ( event == EVENT_RBUTTONDOWN ){
-        cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-    }
-    else if  ( event == EVENT_MBUTTONDOWN ){
-        cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-    }
-    else if ( event == EVENT_MOUSEMOVE ){
-
-
-    }
-}
-
-void drawGraph(vector<double> corrSeries, string WinName){
-    namedWindow(WinName,WINDOW_NORMAL );
-    int sheetSize = 256;
-    Mat graph(sheetSize,sheetSize,CV_64FC1,0.0);
-    int numX = corrSeries.size();
-    int secSize  = (int)(sheetSize/numX);
-    Rect rio;
-    for(int i = 0; i < numX; i++){
-        int height = (int)((1-(corrSeries.at(i)))*sheetSize)-1;
-        int start = i*secSize;
-        std::cout.precision(6);
-        cout << corrSeries.at(i)<<" ";
-        if (height < 0){
-            height = 0;
-        }
-        rio =Rect(start, height, secSize, sheetSize-height);
-        //cout << rio.x << " " << rio.y << " " << rio.width<< " "  << rio.height <<" "<< graph.size() << endl;
-            //graph(rio) = 1.0;
-            rectangle(graph, rio, Scalar(1));
-    }
-    cout <<endl;
-    imshow(WinName, graph);
-}
-
-vector<Point> findCodedness(vector<Mat> corrBuffer,string winName, double threshold, vector<double> codeSeries, bool veryCorr,double factorHigh, double factorLow, vector<Mat>& maxedCorr, Point clickedLocationOld){
-    namedWindow(winName,WINDOW_NORMAL );
-    namedWindow(winName+"2",WINDOW_NORMAL );
-    Mat heatMap(corrBuffer.at(0).size(),CV_64FC1,0.0);
-    Mat heatMapNorm(corrBuffer.at(0).size(),CV_64FC1,0.0);
-    vector<Point> hotSpots;
-    int rows = heatMap.rows;
-    int cols = heatMap.cols;
-    for(int i = 0; i < cols; i++){
-        for(int j=0; j< rows; j++){
-            double sum  = 0;
-            double sum2  = 0;
-            vector<double> corrSeries;
-            for(int k = 0;  k < corrBuffer.size(); k++){
-                corrSeries.push_back((corrBuffer.at(k).at<double>(j,i)));
-            }
-
-            double minCorr = 10e8;
-            double maxCorr = -10e9;
-             for(int p = 0; p <corrBuffer.size(); p++){
-                 if(corrSeries.at(p)>maxCorr){
-                     maxCorr = corrSeries.at(p);
-                 }
-                 if(corrSeries.at(p)<minCorr){
-                     minCorr = corrSeries.at(p);
-                 }
-             }
-            for(int k = 0;  k < corrBuffer.size(); k++){
-
-                corrSeries.at(k) = (corrSeries.at(k) - minCorr)/(maxCorr - minCorr);
-
-            }
-            for(int k = 0;  k < corrBuffer.size(); k++){
-                maxedCorr.at(k).at<double>(j,i) = corrSeries.at(k) ;
-            }
-
-
-            for(int k = 0;  k < corrBuffer.size(); k++){
-                double val = corrSeries.at(k) - codeSeries.at(k);
-                double val2 = corrBuffer.at(k).at<double>(j,i) - codeSeries.at(k);
-                sum += val * val;
-                sum2 += val2 * val2;
-            }
-            heatMap.at<double>(j,i) = (sum);
-            heatMapNorm.at<double>(j,i) =sum2;
-            if(sum > threshold){
-                Point thisHotspot;
-                thisHotspot.x = i;
-                thisHotspot.y = j;
-                hotSpots.push_back(thisHotspot);
-            }
-        }
-    }
-
-
-    double min,max;
-    minMaxLoc(heatMap, &min, &max);
-    double diff = max - min;
-    if(veryCorr){
-        max =  min + (factorHigh*diff);
-        min =  max - (factorLow*diff);
-        for(int i = 0; i < cols; i++){
-            for(int j=0; j< rows; j++){
-                if (heatMap.at<double>(j,i) > max){
-                    heatMap.at<double>(j,i) =  max;
-                }
-                if (heatMap.at<double>(j,i) < min){
-                    heatMap.at<double>(j,i) =  min;
-                }
-            }
-        }
-
-    }
-
-    heatMap = (heatMap - min)/(max - min);
-    min = 10e10;
-    max = -10e10;
-    minMaxLoc(heatMapNorm, &min, &max);
-    cout << min << "        "<< max << endl;
-    if(veryCorr){
-        max =  min + (factorHigh*diff);
-        min =  max - (factorLow*diff);
-        for(int i = 0; i < cols; i++){
-            for(int j=0; j< rows; j++){
-                if (heatMapNorm.at<double>(j,i) > max){
-                    heatMapNorm.at<double>(j,i) =  max;
-                }
-                if (heatMapNorm.at<double>(j,i) < min){
-                    heatMapNorm.at<double>(j,i) =  min;
-                }
-            }
-        }
-
-    }
-    heatMapNorm = (heatMapNorm - min)/(max - min);
-    Mat heatInverse(heatMap.size(),CV_64FC1,1.0);
-    Mat heatInverse2(heatMapNorm.size(),CV_64FC1,1.0);
-    heatInverse -= heatMap;
-    heatInverse2 -= heatMapNorm;
-    cout<<"heatmap value is:"<< heatMap.at<double>(clickedLocationOld)<< endl;
-    cout<<"heatmapNorm value is:"<< heatMapNorm.at<double>(clickedLocationOld)<< endl;
-    imshow(winName, heatInverse);
-    imshow(winName+"2", heatInverse2);
-    return hotSpots;
-}
-
-
-template<unsigned int N>
-vector<double> corrCode(string bitstring, int derating){
-    bitset<N> bits(bitstring);
-    vector<double> input(bits.size()*derating);
-    vector<double> output(input.size(), 0);
-
-    for(size_t i = 0; i != input.size(); i++)
-        input[i] = bits[i/derating];
-
-    for(size_t i = 0; i != input.size(); i++){
-        for(size_t j = 0; j!= input.size(); j++){
-            output[i] += input[j]*input[(j+i)%input.size()];
-        }
-    }
-
-    for(size_t i = input.size(); i !=0; i--)
-        output[i-1] /= output[0];
-
-    return output;
-}
